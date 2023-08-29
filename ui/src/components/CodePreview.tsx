1b1e4c516dcfa387a7cbb81db869df1883e3f711
Sam Calamos <sam.calamos@gmail.com>
2023-08-29 16:40:49 +1000

    fix solidity highlighting

--- ui/src/components/CodePreview.tsx
+++ ui/src/components/CodePreview.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import useGetChallengeData from '../hooks/getChallengeData'
 import "highlight.js/styles/github-dark.css";
 import hljs from "highlight.js";
+import hljsDefineSolidity from "highlightjs-solidity";
 
 export default function CodePreview() {
   const { challengeSource } = useGetChallengeData();
+  hljsDefineSolidity(hljs);
   hljs.highlightAll();
 
   // TODO: Fix this so that you can display multiple contract sources
@@ -15,8 +17,8 @@ export default function CodePreview() {
     <div>
       {/* <h3 className='text-left'>SolveMe.sol</h3> */}
       <div className='bg-zinc-200 dark:bg-zinc-900 rounded p-2 w-160'>
-        <pre className="text-left overflow-y-scroll max-h-128 text-xs">
-          <code>{code2}</code>
+        <pre className="solidity text-left overflow-y-scroll max-h-128 text-xs">
+          <code >{code2}</code>
         </pre>
       </div>
     </div>
